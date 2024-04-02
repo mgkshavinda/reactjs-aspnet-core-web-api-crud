69e3b5d49f0392459b14df62be61e1eab1e8c016
Kavindu Shavinda <kavindu2015@gmail.com>
2024-04-02 21:53:05 +0530

    Completed NavMenu Component
    Completed NavMenu Component

---
+++ Frontend/crud-app/src/Components/NavMenu.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+const NavMenu = () => {
+  return (
+    <div>
+        <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Student Management System</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Details</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+          </Nav>
+        </Container>
+        </Navbar>
+
+    </div>
+  )
+}
+
+export default NavMenu
