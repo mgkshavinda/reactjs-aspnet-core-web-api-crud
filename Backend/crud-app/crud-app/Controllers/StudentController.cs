94a1620df8ded04c3a3ee32c2f0abf432336e7af
Kavindu Shavinda <kavindu2015@gmail.com>
2024-04-03 23:14:56 +0530

    Created Backend Project
    Created Backend Project

---
+++ Backend/crud-app/crud-app/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using crud_app.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud_app.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly StudentContext _studentContext;
+        public StudentController(StudentContext studentContext)
+        {
+            _studentContext = studentContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetStudent()
+        {
+            var student = await _studentContext.Students.ToListAsync();
+            return Ok(student);
+        }
+
+    }
+
+}
